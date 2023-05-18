4487f60309d2134673c7bc153da5a04103e5b7af
shabbirrajput <mohmad.shabbir@9spl.com>
2023-05-18 18:04:22 +0530

    Created core files ---> Done

---
+++ lib/core/app_image.dart
@@ -0,0 +1,4 @@
+class AppImage {
+  static const String filePathImage = 'assets/images/';
+  static const String appWaterDropIcon = '${filePathImage}water_drop_icon.svg';
+}
---
+++ lib/core/app_string.dart
@@ -0,0 +1,3 @@
+class AppString {
+  static const textDripsWater = 'Drips Water';
+}
